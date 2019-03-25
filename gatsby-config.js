a911179e1389784ca8a77234a1e5ccb08c74d796
Mats Grimsgaard <mats@mgv.no>
2019-03-25 20:40:17 +0100

    Testing cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,8 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
   ],
 }
