5e211649ae5322d0c979faa4e0ef21a3a54bdbaf
Mats Grimsgaard <mats@mgv.no>
2019-03-26 21:58:43 +0100

    Config markdown parse

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
