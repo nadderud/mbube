2e09b3193cc6a918cc3048f779160b0f42b86b60
Mats Grimsgaard <mats@mgv.no>
2019-09-26 20:34:34 +0200

    Implement featuredimage on blogpost

--- src/templates/blog-postTemplate.js
+++ src/templates/blog-postTemplate.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import { graphql } from "gatsby"
 
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
-import Byline from '../components/Byline';
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import Byline from "../components/Byline"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data; // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark;
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark
   return (
-    <Layout title={frontmatter.title}>
+    <Layout title={frontmatter.title} image={frontmatter.featuredimage}>
       <SEO title={frontmatter.title} />
       <Byline frontmatter={frontmatter} />
-      <div className="page-content" dangerouslySetInnerHTML={{ __html: html }} />
+      <div
+        className="page-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </Layout>
-  );
+  )
 }
 
 export const pageQuery = graphql`
@@ -26,7 +29,14 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "D. MMMM YYYY", locale: "nb")
         title
+        featuredimage {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
       }
     }
   }
-`;
+`
